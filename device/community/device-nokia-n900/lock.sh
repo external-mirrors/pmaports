01ed02724a2502e2685484b0928e7e2373a6fffe
Sicelo A. Mhlongo <absicsz@gmail.com>
2026-02-15 19:07:10 +0200

    device-nokia-n900: i3wm: engage lock when system idle
    Thus far, when the system became idle, the only action taken was to blank the
    screen.  Using xautolock, it is possible to engage lock.sh so the screen is not
    woken up by accidental touches. Additionally, modify lock.sh so turning the
    screen off also turns the keyboard backlight off.
    
    Signed-off-By: Sicelo A. Mhlongo <absicsz@gmail.com>

--- device/community/device-nokia-n900/lock.sh
+++ device/community/device-nokia-n900/lock.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+set_kbd_backlight() {
+	for i in $(seq 1 6); do
+		brightnessctl --quiet --device="*kb$i" s "$1"
+	done
+}
+
 touch_state=$(xinput list-props "TSC2005 touchscreen" | grep "Device Enabled" | tr -d "\t" | cut -d ":" -f 2)
 
 case "$touch_state" in
 	0)
+		xautolock -enable
 		xinput enable "TSC2005 touchscreen"
 		xset dpms force on
+		set_kbd_backlight 25
 		;;
 	1)
+		xautolock -disable
 		xinput disable "TSC2005 touchscreen"
 		xset dpms force off
+		set_kbd_backlight 0
 		;;
 esac
