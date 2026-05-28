80138612a2cd3821a0663ac4980dab8c0c7bf2f1
Oliver Smith <ollieparanoid@postmarketos.org>
2026-05-28 06:37:27 +0200

    Revert "CI: tethering script is removed"
    This reverts commit c5a8b1b6cd6226251d5fa4f4ca3a11087aa338c8.
    
    Signed-off-by: Oliver Smith <ollieparanoid@postmarketos.org>

--- .ci/shellcheck.sh
+++ .ci/shellcheck.sh
@@ -16,6 +16,7 @@ fi
 sh_files="
 	./main/mdss-fb-init-hack/mdss-fb-init-hack.sh
 	./main/postmarketos-base-ui/rootfs-usr-lib-NetworkManager-dispatcher.d-50-dns-filter.sh
+	./main/postmarketos-base-ui/rootfs-usr-lib-NetworkManager-dispatcher.d-50-tethering.sh
 	./main/postmarketos-duranium/rootfs-usr-bin-duranium-set-channel
 	./main/postmarketos-duranium/rootfs-usr-libexec-duranium-first-boot
 	./main/postmarketos-duranium/rootfs-usr-libexec-duranium-mount-subpartitions
