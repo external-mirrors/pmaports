130488faf491eebe7092c2b40e623ae5f576e84e
Ferass El Hafidi <funderscore@postmarketos.org>
2026-05-30 17:38:29 +0000

    soc-amlogic-aarch64: update script to use boot-deploy's environ.txt
    Signed-off-by: Ferass El Hafidi <funderscore@postmarketos.org>

--- device/testing/soc-amlogic-aarch64/s905_autoscript.cmd
+++ device/testing/soc-amlogic-aarch64/s905_autoscript.cmd
@@ -3,8 +3,8 @@ if fatload usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if ext4load mmc 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if ext4load usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 setenv pmos_env_addr 0x1040000
-setenv pmos_env_load 'fatload ${pmos_devtype} ${pmos_devnum} ${pmos_env_addr} uEnv.ini && env import -t ${pmos_env_addr} ${filesize}'
-setenv pmos_try_uimage 'if fatload ${pmos_devtype} ${pmos_devnum} ${loadaddr} uImage; then fatload ${pmos_devtype} ${pmos_devnum} ${dtb_mem_addr} ${pmos_env_dtb} && bootm ${loadaddr} - ${dtb_mem_addr}; fi'
+setenv pmos_env_load 'fatload ${pmos_devtype} ${pmos_devnum} ${pmos_env_addr} environ.txt && env import -t ${pmos_env_addr} ${filesize}'
+setenv pmos_try_uimage 'if fatload ${pmos_devtype} ${pmos_devnum} ${loadaddr} uImage; then fatload ${pmos_devtype} ${pmos_devnum} ${dtb_mem_addr} ${devicetree} && bootm ${loadaddr} - ${dtb_mem_addr}; fi'
 setenv pmos_try_fit 'if fatload ${pmos_devtype} ${pmos_devnum} ${loadaddr} boot-image.itb; then bootm ${loadaddr}; fi'
 setenv pmos_try_boot 'run pmos_try_fit; run pmos_try_uimage'
 setenv pmos_devtype usb
